7171e28440b743db9e36612f0f2c96e2df8a3970
limzibinxd <limzibinxd@users.noreply.github.com>
2025-08-27 09:23:58 +0000

    deploy: 2d739ac38715e221fb427ae298024e0c6c25a0df

---
+++ assets/js/5e95c892.b6e6ce2d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmy_tutorial=self.webpackChunkmy_tutorial||[]).push([[9647],{7121:(e,r,s)=>{s.r(r),s.d(r,{default:()=>i});s(6540);var t=s(4164),u=s(7559),a=s(1213),c=s(2831),l=s(781),n=s(4848);function i(e){return(0,n.jsx)(a.e3,{className:(0,t.A)(u.G.wrapper.docsPages),children:(0,n.jsx)(l.A,{children:(0,c.v)(e.route.routes)})})}}}]);
